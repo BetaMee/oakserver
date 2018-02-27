5573e8a77fc3f8e4823752cd71ecf5a0bf7a0a56
betamee <gongxq95@gmail.com>
2018-02-27 23:36:53 +0800

    update author controller

--- server/controllers/author.js
+++ server/controllers/author.js
@@ -1,3 +1,8 @@
+import uuidv4 from 'uuid/v4'
+import { AuthorModel } from '../models'
+import { getNowTimeStr } from '../utils'
+
+
 /**
  * 获取所有作者
  * @param {*} ctx 
@@ -13,7 +18,24 @@ const fetchAuthors = async (ctx, next) => {
  * @param {*} next 
  */
 const fetchAuthorById = async (ctx, next) => {
-
+  const authorId = ctx.params.authorId
+  try {
+    const author = await AuthorModel.getByAuthorId(authorId)
+    const result = {
+      action: 'GET',
+      message: 'get author successfully',
+      success: true,
+      item: author.Item
+    }
+    ctx.body = result
+  } catch (e) {
+    const error = {
+      message: e.message,
+      code: e.code,
+      success: false
+    }
+    ctx.body = error
+  }
 }
 
 /**
@@ -22,7 +44,41 @@ const fetchAuthorById = async (ctx, next) => {
  * @param {*} next 
  */
 const createAuthor = async (ctx, next) => {
-  
+  // 获取body中的数据
+  const {
+    name,
+    gender,
+    email,
+    social,
+    avatar,
+  } = ctx.request.body
+  // 生成作者唯一ID
+  const authorId = uuidv4()
+  // 生成时间戳
+  const nowTime = getNowTimeStr(new Date())
+  const createdAt = nowTime
+  const updatedAt = nowTime
+  // 写入数据库
+  const newAuthor = {
+    authorId, name, gender, email, social, avatar, createdAt, updatedAt
+  }
+  try {
+    await AuthorModel.create(newAuthor)
+    const result = {
+      action: 'CREATE',
+      message: 'article author successfully',
+      success: true,
+      item: newAuthor
+    }
+    ctx.body = result
+  } catch(e) {
+    const error = {
+      message: e.message,
+      code: e.code,
+      success: false
+    }
+    ctx.body = error
+  }
 }
 
 /**
@@ -31,7 +87,44 @@ const createAuthor = async (ctx, next) => {
  * @param {*} next 
  */
 const updateAuthorById = async (ctx, next) => {
-
+  // 获取文章ID
+  const authorId = ctx.params.authorId
+  // 更新updateAt
+  const updatedAt = getNowTimeStr(new Date())
+  // 获取可以更新的字段
+  const {
+    name,
+    gender,
+    email,
+    social,
+    avatar,
+  } = ctx.request.body
+  // 更新的对象
+  const toUpdateAuthor = {
+    name,
+    gender,
+    email,
+    social,
+    avatar,
+    updatedAt
+  }
+  try {
+    const updatedAuthor = await AuthorModel.updateByAuthorId(authorId, toUpdateAuthor)
+    const result = {
+      action: 'UPDATE',
+      message: 'author updated successfully',
+      success: true,
+      item: updatedAuthor.Attributes
+    }
+    ctx.body = result
+  } catch(e) {
+    const error = {
+      message: e.message,
+      code: e.code,
+      success: false
+    }
+    ctx.body = error
+  }
 }
 
 
