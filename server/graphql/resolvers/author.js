09f33d4ea047ff06a238b3d9f2171e1ef82058b2
betamee <gongxq95@gmail.com>
2018-03-20 23:35:08 +0800

    update resolvers

--- server/graphql/resolvers/author.js
+++ server/graphql/resolvers/author.js
@@ -1,8 +1,20 @@
 const authorResolverMap = {
   Author: {
-    article: (author, args, context) => {
+    articles: async (author, args, context) => {
+      const { ArticleModel } = context.DBModel
       const { authorId } = author
       // 通过authorId来查询对应文章
+      try {
+        const result = await ArticleModel.queryByAuthorId(authorId)
+        if (result.Count === 0) {
+          return []
+        } else {
+          return result.Items
+        }
+      } catch(e) {
+        console.log(e)
+        return []
+      }
     }
   }
 }
--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -5,11 +5,19 @@ import authorResolverMap from './author'
 const rootResolverMap = {
   // 根字段查询
   Query: {
-    article: async (obj, args, context, info) => {
-      const { ArticleModel } = context.DBModel
-      const { articleId } = args
-      const article = await ArticleModel.getByArticleId(articleId)
-      return article.Item
+    author: async (obj, args, context, info) => {
+      const { AuthorModel } = context.DBModel
+      const { authorId } = args
+      try {
+        const author = await AuthorModel.getByAuthorId(authorId)
+        let Item = null
+        if (author.Item) {
+          Item = author.Item
+        }
+        return Item
+      } catch(e) {
+        return null
+      }
     },
 
     archive: async (obj, args, context, info) => {
@@ -19,12 +27,24 @@ const rootResolverMap = {
       return archive.Item
     },
 
-    author: async (obj, args, context, info) => {
-      const { AuthorModel } = context.DBModel
-      const { authorId } = args
-      const author = await AuthorModel.getByAuthorId(authorId)
-      return author.Item
-    }
+    article: async (obj, args, context, info) => {
+      const { ArticleModel } = context.DBModel
+      const { articleId } = args
+      const article = await ArticleModel.getByArticleId(articleId)
+      return article.Item
+    },
+
+    articles: async (obj, args, context, info) => {
+
+    },
+
+    authors:  async (obj, args, context, info) => {
+
+    },
+
+    archives:  async (obj, args, context, info) => {
+
+    },
   }
 }
 
